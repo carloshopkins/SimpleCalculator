a3e0227564af7855d6bfdeb0d153a9c2809f7a92
Kuhgelfische <Kuhgelfische@DESKTOP-SKJVMS6>
2017-03-24 11:41:19 -0400

    Add project files.

---
+++ SimpleCalculator/Default.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="SimpleCalculator.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <br />
+    
+    </div>
+        <h1>Simple Calculator</h1>
+        <p>
+            First Value:
+            <asp:TextBox ID="firstField" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            Second Value:
+            <asp:TextBox ID="secondField" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Button ID="plusButton" runat="server" OnClick="plusButton_Click" Text="+" />
+&nbsp;
+            <asp:Button ID="minusButton" runat="server" OnClick="minusButton_Click" Text="-" />
+&nbsp;
+            <asp:Button ID="multiplyButton" runat="server" OnClick="multiplyButton_Click" Text="*" />
+&nbsp;
+            <asp:Button ID="divideButton" runat="server" OnClick="divideButton_Click" style="width: 20px" Text="/" />
+        </p>
+        <p>
+            &nbsp;</p>
+        <p>
+            <asp:Label ID="resultLabel" runat="server" Text="Label"></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
---
+++ SimpleCalculator/Default.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SimpleCalculator
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+        public static void initValues()
+        {
+            
+        }
+
+        protected void plusButton_Click(object sender, EventArgs e)
+        {
+            //Find a way to do this in a method
+            int a = int.Parse(firstField.Text);
+            int b = int.Parse(secondField.Text);
+
+            resultLabel.Text = (a + b).ToString();
+        }
+
+        protected void minusButton_Click(object sender, EventArgs e)
+        {
+            //Find a way to do this in a method
+            int a = int.Parse(firstField.Text);
+            int b = int.Parse(secondField.Text);
+
+            resultLabel.Text = (a - b).ToString();
+        }
+
+        protected void multiplyButton_Click(object sender, EventArgs e)
+        {
+            //Find a way to do this in a method
+            int a = int.Parse(firstField.Text);
+            int b = int.Parse(secondField.Text);
+
+            resultLabel.Text = (a * b).ToString();
+        }
+
+        protected void divideButton_Click(object sender, EventArgs e)
+        {
+            //Find a way to do this in a method
+            //Could add a remainder
+            int a = int.Parse(firstField.Text);
+            int b = int.Parse(secondField.Text);
+
+            resultLabel.Text = (a / b).ToString();
+        }
+    }
+}
